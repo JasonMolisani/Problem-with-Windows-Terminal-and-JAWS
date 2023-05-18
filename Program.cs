8bda861bdd09b9bd3a7502f922dd9d7f6f6fd146
JasonMolisani <jmolisani@gmail.com>
2023-05-18 13:46:51 -0500

    Add minimal project for reproduction

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.CommandLine;
+using Spectre.Console;
+
+namespace scl;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        Option<int?> delayOption = new(
+            name: "--delay",
+            description: "The time in milliseconds to wait between printing each color. The default is 10ms.");
+
+        var rootCommand = new RootCommand("Simple command that prints the colors in the ConsoleColor enum");
+        rootCommand.AddOption(delayOption);
+
+        rootCommand.SetHandler((delay) =>
+            {
+                PrintColors(delay ?? 10);
+            },
+            delayOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static void PrintColors(int delay)
+    {
+        foreach (ConsoleColor color in (ConsoleColor[])Enum.GetValues(typeof(ConsoleColor)))
+        {
+            AnsiConsole.MarkupLine($"[{(int)color}]{color}[/]");
+            Thread.Sleep(delay);
+        }
+    }
+}
